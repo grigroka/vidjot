bbdfd6f6ad777efb2cbc4908b931b5523c02c5d6
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-05 17:22:00 +0200

    save user to db

--- routes/users.js
+++ routes/users.js
@@ -39,27 +39,37 @@ router.post('/register', (req, res) => {
       password2: req.body.password2
     });
   } else {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        req.flash('error_msg', 'Email already registered');
+        res.redirect('/users/register');
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then(user => {
-            req.flash('success_msg', 'You are now registered and can log in');
-            res.redirect('/users/login');
-          })
-          .catch(err => {
-            console.log(err);
-            return;
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => {
+                console.log(err);
+                return;
+              });
           });
-      });
+        });
+      }
     });
   }
 });
