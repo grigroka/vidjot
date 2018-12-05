03b1409d8f36d7d61a612403a0b6ca82c2ff9796
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-05 10:28:53 +0200

    basic routing

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 'use strict';
 const express = require('express');
 const app = express();
+
+// Index route
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+// About route
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+});
+
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
