61a36df8bcd3d5a88f4afdf24b43882b4b6d6e4c
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-05 12:59:35 +0200

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict';
-// Init ExpressJs
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 const app = express();
+
+// Connect to mongoose
+mongoose
+  .connect(
+    'mongodb://localhost:27017/vidjot-dev',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Handlebars middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -19,5 +28,5 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-const port = 5000;
+const port = 8080;
 app.listen(port, () => console.log(`Server started on port ${port}`));
