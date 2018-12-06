e62ee117994a3f0a45519bc7ef6543373addd738
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 10:47:14 +0200

    changed mlab user

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 'use strict';
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://rokas:FYCLmx@9Z001@ds257590.mlab.com:57590/my-vidjot'
+    mongoURI: 'mongodb://testuser:testuser1@ds257590.mlab.com:57590/my-vidjot'
   };
 } else {
   module.exports = {
