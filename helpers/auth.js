d6b161ce0ac964bcb3d6bdbc1aeb4fb43c7fa639
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 10:00:35 +0200

    ensureAuthenticated helper function

---
+++ helpers/auth.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+  ensureAuthenticated: function(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_msg', 'Not Authorized');
+    res.redirect('/users/login');
+  }
+};
