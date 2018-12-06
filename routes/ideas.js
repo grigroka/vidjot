50225676f89bf6096f147edcb014415eb6d21db8
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 10:05:59 +0200

    protect idea routes

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,18 +2,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const { ensureAuthenticated } = require('../helpers/auth');
 
 // Load Idea model
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
 // Add Idea Form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('ideas/add');
 });
 
 // Edit Idea Form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
@@ -22,7 +23,7 @@ router.get('/edit/:id', (req, res) => {
 });
 
 // Idea Index route
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
   Idea.find({})
     .sort({ date: 'desc' })
     .then(ideas => {
@@ -31,7 +32,7 @@ router.get('/', (req, res) => {
 });
 
 // Process Idea Form
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
   let errors = [];
   if (!req.body.title) {
     errors.push({ text: 'Please add a title' });
@@ -59,7 +60,7 @@ router.post('/', (req, res) => {
 });
 
 // Edit Form process
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
@@ -75,7 +76,7 @@ router.put('/:id', (req, res) => {
 });
 
 // Delete Idea
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
   Idea.remove({ _id: req.params.id }).then(() => {
     req.flash('success_msg', 'Video idea removed');
     res.redirect('/ideas');
