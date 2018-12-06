7a1dc1ebf61daadcc16f950637ba48576491dc98
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 10:16:05 +0200

    access control for ideas

--- routes/ideas.js
+++ routes/ideas.js
@@ -18,13 +18,18 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
-    res.render('ideas/edit', { idea });
+    if (idea.user !== req.user.id) {
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', { idea });
+    }
   });
 });
 
 // Idea Index route
 router.get('/', ensureAuthenticated, (req, res) => {
-  Idea.find({})
+  Idea.find({ user: req.user.id })
     .sort({ date: 'desc' })
     .then(ideas => {
       res.render('ideas/index', { ideas });
@@ -50,7 +55,8 @@ router.post('/', ensureAuthenticated, (req, res) => {
   } else {
     const newUser = {
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     };
     new Idea(newUser).save().then(idea => {
       req.flash('success_msg', 'Video idea added');
