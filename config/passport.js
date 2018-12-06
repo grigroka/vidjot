5ab467a22c805ef5f8d226330bdb02843af64b94
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 09:39:50 +0200

    Match user password

--- config/passport.js
+++ config/passport.js
@@ -9,12 +9,22 @@ const User = mongoose.model('users');
 module.exports = function(passport) {
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      // Match user
       User.findOne({
         email
       }).then(user => {
         if (!user) {
           return done(null, false, { message: 'No User Found' });
         }
+        // Match password
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password Incorrect' });
+          }
+        });
       });
     })
   );
